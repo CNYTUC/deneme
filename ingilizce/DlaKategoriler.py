860ce9e0d74d62de61c9f2e0f31a68aa8d76cb54
CNYTUC <cuneytuc@gmail.com>
2026-04-28 23:31:10 +0800

    5

--- ingilizce/DlaKategoriler.py
+++ ingilizce/DlaKategoriler.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from supabaseFonksiyon import dla_kategori_ekle 
+from supabaseFonksiyon import dla_kategori_ekle
 
 #BAŞLIK
 #============================================================================================
