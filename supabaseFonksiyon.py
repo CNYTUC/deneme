e1384553af6298b5f27643a3bbb8b42564c4caf2
CNYTUC <cuneytuc@gmail.com>
2026-04-28 23:29:50 +0800

    5

--- supabaseFonksiyon.py
+++ supabaseFonksiyon.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from supabase import create_client
+from supabaseFonksiyon import create_client
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
