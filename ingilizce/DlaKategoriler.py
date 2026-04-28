294cfb1a16490a2e9c1a52f53c7e8d95d1476fab
CNYTUC <cuneytuc@gmail.com>
2026-04-28 22:51:40 +0800

    3

--- ingilizce/DlaKategoriler.py
+++ ingilizce/DlaKategoriler.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from supabase import create_client
 
+#BAŞLIK
+#============================================================================================
 st.title("Dla Kategori Editörü")
 st.write("Dla Kategori Editörü sayfasına hoş geldiniz. Bu sayfa üzerinden DLA sınav kategorilerini yönetebilirsiniz.")
+
+
 
 tab1, tab2, tab3,  tab4  = st.tabs(["Görüntüle", "Ekle", "Düzenle", "Sil"])
 
@@ -11,17 +15,32 @@ with tab1:
 with tab2:
     st.header("Yeni Kategori Ekle")
 
-    st.radio(
-        "Kategori Seçin 👉",
-        key="visibility",
-        options=["General", "Scenario", "PictureDescription"],
-    )
-    Dla_Alt_Kategori = st.text_input(
-        "Alt Kategori Oluşturun 👇",
-        placeholder="Örnek: General Test 1",
-        key="Dla_alt_kategori_input",
-    )
+# Kategori seçimi oluştur.
+col1, col2 = st.columns([2, 1])
+
+with col1:
+    with st.container(border=True,vertical_alignment="center",height=200):
+        Ana_Kategori =st.radio(
+            "Kategori Seçin 👉",
+            key="visibility",
+            options=["General", "Scenario", "PictureDescription"],
+        )
+
+with col2:
+    with st.container(border=True,vertical_alignment="center",height=200):
+        Dla_Alt_Kategori = st.text_input(
+            "Alt Kategori Oluşturun 👇",
+            placeholder="Örnek: Prefer",
+            key="Dla_alt_kategori_input",
+        )
     
+    load_Clicked = st.button(
+            "Load Questions",
+            key="Dla_Kategori_Input",
+            icon=":material/published_with_changes:",
+            use_container_width=True
+        )
+
 with tab3:
     st.header("Kategori Düzenle")
 with tab4:
