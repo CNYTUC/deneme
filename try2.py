a783d9a2dcb23e776a80676e587d4eb15c38b85c
CNYTUC <cuneytuc@gmail.com>
2026-05-02 15:12:50 +0300

    4

--- try2.py
+++ try2.py
@@ -196,9 +196,9 @@ with tab2:
                 hide_index=True,
                 row_height=42,
                 column_config={
-                    "sec": st.column_config.CheckboxColumn("SEC", width="small"),
+                    "sec": st.column_config.CheckboxColumn("SEC", width=100),
                     "id": None,  # 👈 BU SATIR KOLONU GİZLER
-                    "Etiket": st.column_config.TextColumn("ETİKET", width="large"),
+                    "Etiket": st.column_config.TextColumn("ETİKET"),
                 },
                 key="MEK_etiket_editor"
             )
