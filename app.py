ddb90eb740b35a4115909f033f377dc5bbebaf55
CNYTUC <cuneytuc@gmail.com>
2026-05-07 08:48:16 +0300

    1

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ pages = {
     
     "MAIN": [
         st.Page("main.py", title="Main", icon="🏠"),
-        st.Page("try.py", title="try", icon="🧪"),
-        st.Page("pages/1_dla_editor.py", title="try2", icon="🧪"),
-        st.Page("pages/01_dla_editor.py", title="try3", icon="🧪"),
+        # st.Page("try.py", title="try", icon="🧪"),
+        # st.Page("pages/1_dla_editor.py", title="try2", icon="🧪"),
+        st.Page("pages/01_dla_editor.py", title="try", icon="🧪"),
     ],     
     "DLA SINAVLARI": [
-        st.Page("ingilizce/general.py", title="General Test", icon="🎤"),
+        # st.Page("ingilizce/general.py", title="General Test", icon="🎤"),
     ],     
     "DLA EDİTÖRLERİ": [
         # st.Page("pages/dla_1_kategoriler/dla_kategoriEditor.py", title="Kategori Editörü", icon="📚"),
