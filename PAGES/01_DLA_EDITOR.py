a18d427f2cc9594886301dd91caca50cd5ca70b3
CNYTUC <cuneytuc@gmail.com>
2026-05-08 12:40:52 +0300

    222

--- PAGES/01_DLA_EDITOR.py
+++ PAGES/01_DLA_EDITOR.py
@@ -9,9 +9,6 @@ from io import BytesIO
 # import UTILS.text_utils as TxtFonk
 # import UTILS.time_utils as TimeFonk
 
-# İCERİK import
-import icerikler as icerik
-
 # BAŞLIK
 # ============================================================================================
 st.header("D.L.A. Editörü 🤠")
@@ -20,97 +17,97 @@ st.header("D.L.A. Editörü 🤠")
 # ============================================================================================
 Yeni_Soru, Mevcut_Soru, Mevcut_Etiketler = st.tabs(["❓ Yeni Soru", "📖 Mevcut Sorular", "🔖 Mevcut Etiketler"])    
 
-# YENİ SORU EKLEME
-# ============================================================================================
-with Yeni_Soru:   
+# # YENİ SORU EKLEME
+# # ============================================================================================
+# with Yeni_Soru:   
     
-    # ALT BAŞLIK BELİRLE
-    # ============================================================================================
-    st.subheader(f"Yeni Soru Ekle ❓",divider="yellow")
+#     # ALT BAŞLIK BELİRLE
+#     # ============================================================================================
+#     st.subheader(f"Yeni Soru Ekle ❓",divider="yellow")
     
-    # DEĞİŞKENLER
-    # ============================================================================================
-    Ana_kategoriler = st.session_state.Ana_kategoriler_list
-    Secilen_ana_kategori: str = ""
+#     # DEĞİŞKENLER
+#     # ============================================================================================
+#     Ana_kategoriler = st.session_state.Ana_kategoriler_list
+#     Secilen_ana_kategori: str = ""
 
-    # DIŞ CONTAINER OLUSTUR
-    # ============================================================================================
-    with st.container(border=True,vertical_alignment="center",height="stretch"):
+#     # DIŞ CONTAINER OLUSTUR
+#     # ============================================================================================
+#     with st.container(border=True,vertical_alignment="center",height="stretch"):
         
-        # Ana kategori seçimi
-        # ============================================================================================  
-        # with st.container(border=True, vertical_alignment="center", height="stretch"):
+#         # Ana kategori seçimi
+#         # ============================================================================================  
+#         # with st.container(border=True, vertical_alignment="center", height="stretch"):
 
 
-        Secilen_ana_kategori = st.radio(
-            "Ana Kategori",
-            options=Ana_kategoriler,
-            key="YSK_ana_kategori",
-            horizontal=True
-        )
+#         Secilen_ana_kategori = st.radio(
+#             "Ana Kategori",
+#             options=Ana_kategoriler,
+#             key="YSK_ana_kategori",
+#             horizontal=True
+#         )
 
-        #Önerme Yaz
-        if Secilen_ana_kategori == "PictureDescription":
-            st.write("Gereklilikler: En az 1 Etiket, Sadece 1 Soru metni ve 1 Resim yolu.")
-        else:
-            st.write("Gereklilikler: En az 1 Etiket, Soru metni.")
+#         #Önerme Yaz
+#         if Secilen_ana_kategori == "PictureDescription":
+#             st.write("Gereklilikler: En az 1 Etiket, Sadece 1 Soru metni ve 1 Resim yolu.")
+#         else:
+#             st.write("Gereklilikler: En az 1 Etiket, Soru metni.")
 
-        # st.write(Secilen_ana_kategori)
+#         # st.write(Secilen_ana_kategori)
 
 
-        # Etiketler girişi
-        # ============================================================================================
+#         # Etiketler girişi
+#         # ============================================================================================
 
-        VT_ETIKETLER = SpFonk.dla_etiketler_DF()
-        mevcut_etiketler_seti = set(VT_ETIKETLER["Etiket"].dropna().unique())
-        YS_ETIKETLER: list = []
+#         VT_ETIKETLER = SpFonk.dla_etiketler_DF()
+#         mevcut_etiketler_seti = set(VT_ETIKETLER["Etiket"].dropna().unique())
+#         YS_ETIKETLER: list = []
         
-        # 1. Etiketler seçme
-        # with st.container(border=True, vertical_alignment="center", height="stretch"):
+#         # 1. Etiketler seçme
+#         # with st.container(border=True, vertical_alignment="center", height="stretch"):
             
-        YS_ETIKETLER = st.multiselect(
-            "Etiketlerinizi seçin",
-            options=mevcut_etiketler_seti,
-            max_selections=20,
-            accept_new_options=True,
-            placeholder="Etiketlerinizi seçin !!!",
-            key="YSK_etiketler0",
-            )
+#         YS_ETIKETLER = st.multiselect(
+#             "Etiketlerinizi seçin",
+#             options=mevcut_etiketler_seti,
+#             max_selections=20,
+#             accept_new_options=True,
+#             placeholder="Etiketlerinizi seçin !!!",
+#             key="YSK_etiketler0",
+#             )
                 
-        # Etiketleri yazdır
-        st.write(TxtFonk.tr_to_en_lower(", ".join(YS_ETIKETLER)))
+#         # Etiketleri yazdır
+#         st.write(TxtFonk.tr_to_en_lower(", ".join(YS_ETIKETLER)))
     
  
 
     
     
-# MEVCUT SORULAR
-# ============================================================================================
-with Mevcut_Soru:
+# # MEVCUT SORULAR
+# # ============================================================================================
+# with Mevcut_Soru:
 
-    # ALT BAŞLIK BELİRLE
-    # ============================================================================================
-    st.subheader(f"Mevcut Etiketler 🔖",divider="rainbow")
+#     # ALT BAŞLIK BELİRLE
+#     # ============================================================================================
+#     st.subheader(f"Mevcut Etiketler 🔖",divider="rainbow")
 
-    # DIŞ CONTAINER OLUSTUR
-    # ============================================================================================
-    with st.container(border=True,vertical_alignment="center",height="stretch"):
+#     # DIŞ CONTAINER OLUSTUR
+#     # ============================================================================================
+#     with st.container(border=True,vertical_alignment="center",height="stretch"):
         
-        Mevcut_Soru_Alan = st.empty()
+#         Mevcut_Soru_Alan = st.empty()
     
-# MEVCUT ETİKETLER
-# ============================================================================================
-with Mevcut_Etiketler:
+# # MEVCUT ETİKETLER
+# # ============================================================================================
+# with Mevcut_Etiketler:
 
-    # ALT BAŞLIK BELİRLE
-    # ============================================================================================
-    st.subheader(f"Mevcut Sorular 📖",divider="red")
+#     # ALT BAŞLIK BELİRLE
+#     # ============================================================================================
+#     st.subheader(f"Mevcut Sorular 📖",divider="red")
 
-    # DIŞ CONTAINER OLUSTUR
-    # ============================================================================================
-    with st.container(border=True,vertical_alignment="center",height="stretch"):
+#     # DIŞ CONTAINER OLUSTUR
+#     # ============================================================================================
+#     with st.container(border=True,vertical_alignment="center",height="stretch"):
         
-        Mevcut_Etiket_Alan = st.empty()
+#         Mevcut_Etiket_Alan = st.empty()
 
 
 # icerik.Yeni_Soru_Alan_Doldur(Yeni_Soru_Alan)
