8da7d8c0f11a12fbb3924f9e10550f63ef7493ad
CNYTUC <cuneytuc@gmail.com>
2026-05-18 15:46:43 +0300

    678

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import UTILS.text_utils as c_txt
 import UTILS.time_utils as c_time
 
-st.title("🏠 Main")
+st.title("🏠 Hoşgeldiniz")
 
 MetinA = "Strateji Test Sayfasına Hoşgeldiniz."
-MetinB = "Bu sistem kişisel işlemler iç.in tasarelanmıştır"
+MetinB = "Bu sistem kişisel işlemler için tasarelanmıştır"
 
 c_time.wait(1)
 c_txt.slow_print(MetinA,False,0.1)
