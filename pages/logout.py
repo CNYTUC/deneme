e2fe38c6cc3acabcb9ecb89da49e2f3ee50c7f53
CNYTUC <cuneytuc@gmail.com>
2026-05-18 15:58:02 +0300

    00

--- pages/logout.py
+++ pages/logout.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-st.warning("Uygulamadan çıkış yapılıyor... Lütfen bekleyin.")
+st.warning("Uygulamadan çıkış yapılıyor...")
 
-# Bahsettiğiniz yönlendirme komutu
-st.markdown(
-    '<meta http-equiv="refresh" content="0; url=https://www.google.com" />',
-    unsafe_allow_html=True,
-)
+# JavaScript kullanarak tarayıcının en üst penceresini (window.top) yönlendiriyoruz
+js_kod = """
+<script>
+    window.top.location.href = "https://google.com";
+</script>
+"""
+
+st.components.v1.html(js_kod, height=0, width=0)
