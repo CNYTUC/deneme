af65ea7d919caa060a2fe6656a3f665fa93a8d1c
CNYTUC <cuneytuc@gmail.com>
2026-05-18 16:12:03 +0300

    222

--- pages/00StrCreate.py
+++ pages/00StrCreate.py
@@ -1,5 +1,18 @@
 # KÜTÜPHANELER
 import streamlit as st
 
-st.container()
-st.title("Strateji Olusturma Sayfası")
+# BAŞLIK
+# ============================================================================================
+st.header("Yeni Strateji Olustur 🤠")
+
+
+# DIŞ CONTAINER OLUSTUR
+# ============================================================================================
+with st.container(border=True,vertical_alignment="center",height="stretch"):
+    
+    # ALT BAŞLIK BELİRLE
+    # ============================================================================================
+    st.subheader(f"Yeni Soru Ekle ❓",divider="yellow")
+        
+
+        
