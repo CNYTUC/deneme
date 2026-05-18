c600d70cc0db9092614045644a2670ea76cf0afe
CNYTUC <cuneytuc@gmail.com>
2026-05-18 16:32:48 +0300

    55

--- pages/00StrCreate.py
+++ pages/00StrCreate.py
@@ -25,15 +25,17 @@ with st.container(border=True,vertical_alignment="center",height="stretch"):
 
         st.write("Indicatorler")        
 
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            
+            # Onay kutusunu oluşturuyoruz
+            RSI_BOX = st.checkbox("RSI Aktif")
 
-        # Onay kutusunu oluşturuyoruz
-        RSI_BOX = st.checkbox("RSI Aktif")
-
-        # Eğer onay kutusu işaretlendiyse altındaki container açılır
-        if RSI_BOX:
-            portfoy = st.number_input(
-                "Portföy (TL)", min_value=10000, max_value=10000000,
-                value=950000, step=10000
-            )
-            risk_yuzde = st.slider("Risk % (1R)", 0.5, 5.0, 1.0, 0.5)
+            # Eğer onay kutusu işaretlendiyse altındaki container açılır
+            if RSI_BOX:
+                portfoy = st.number_input(
+                    "Portföy (TL)", min_value=10000, max_value=10000000,
+                    value=950000, step=10000
+                )
+                risk_yuzde = st.slider("Risk % (1R)", 0.5, 5.0, 1.0, 0.5)
 
