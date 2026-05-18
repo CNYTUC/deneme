6aac35c770d6e68ed0c0eb19b580cc8e199aa947
CNYTUC <cuneytuc@gmail.com>
2026-05-18 16:46:08 +0300

    654

--- pages/00StrCreate.py
+++ pages/00StrCreate.py
@@ -33,6 +33,14 @@ with st.container(border=True,vertical_alignment="center",height="stretch"):
 
             # Eğer onay kutusu işaretlendiyse altındaki container açılır
             if RSI_BOX:
+                
+                RSI_LEN = st.number_input("RSI Length", min_value=2, max_value=100, value=14, step=1) # RSI Length: 14
+                RSI_Source = st.selectbox("RSI Source", ["Close", "Open"], index=0) # RSI Source: Close
+                RSI_Overbought= st.number_input("RSI Overbought", min_value=0, max_value=100, value=70, step=1) # RSI Overbought: 70
+                RSI_Oversold= st.number_input("RSI Oversold", min_value=0, max_value=100, value=30, step=1) # RSI Oversold: 30
+                RSI_Signal = st.number_input("RSI Signal", min_value=2, max_value=100, value=14, step=1) # RSMA Length (Signal): 14
+                
+            
                 # portfoy = st.number_input(
                 #     "Portföy (TL)", min_value=10000, max_value=10000000,
                 #     value=950000, step=10000
